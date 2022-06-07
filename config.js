90d35ab805a9d948b0685b5b0acc145a4a449901
Tín Lưu <107022820+tinluuVTL@users.noreply.github.com>
2022-06-07 19:25:16 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
+    introDesc: `Tao có điều này muốn hỏi Mày, Mầyyy nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'OK',
+    title: 'Tao biết Mầyy thích Tao phải không phải không ? 😙',
+    desc: 'Mầy không trả lời hoặc thoát ra, tức là Mầyy thích tao đó nha!😁😀 :> ',
+    btnYes: 'Có, Taoo thật sự thích Mầyy 💖💖💖',
+    btnNo: '💔 màyy mơ à ! 💔 ',
+    question:'Nói cho Tao biết đi vì sao màyy thích tao 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì anh đẹp trai đó Ahiiiii 😊😊',
+    mess: 'tao biết mà,Ai cũng nới vậy hết😎. mà tao cũng thích mày lắm 🥰. Love you 300.000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho tao nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://www.messenger.com/t/100082063023921'
+}
